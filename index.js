4b3201657188b55b77c63d307ac62514302267cd
Jared <jaralleg@amazon.com>
2021-04-14 11:13:41 -0400

    addition of 2nd function and preventDefault

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 window.addEventListener('DOMContentLoaded', (event) => {
     console.log('DOM fully loaded and parsed');
 
-const searchBtn = document.querySelector("#github-form > input[type=submit]:nth-child(2)")
-searchBtn.addEventListener('click', searchForUser)
+    document.getElementById("myAnchor").addEventListener("click", function(event){
+        event.preventDefault()
+      });
+    const searchBtn = document.querySelector("#github-form > input[type=submit]:nth-child(2)");
+    searchBtn.addEventListener('click', searchForUser)
+            {searchForUser.preventDefault();}
 
-function searchForUser(){
-   let user = document.getElementById('search').value 
-   fetch('https://api.github.com/search/users?q='+user, () => {
-        console.log(response)
-    })
-    let newLi = document.createElement("li");
-    newLi.innerHTML = user;
-    document.getElementById("user-list").appendChild(newLi);  
-}
+    function searchForUser(){
+    let user = document.getElementById('search').value 
+    fetch('https://api.github.com/search/users?q='+user, () => {
+            console.log(response)
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+        let newLi = document.createElement("li");
+        newLi.innerHTML = user;
+        document.getElementById("user-list").appendChild(newLi);  
+    }
+
+    function searchForRepo(){
+        let user = document.getElementById('search').value 
+        fetch('https://api.github.com/users/'+user+'/repos', () => {
+            console.log(response)
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+        let newLi = document.createElement("li");
+        newLi.innerHTML = user;
+        document.getElementById("repo-list").appendChild(newLi);  
+    }
+
 });

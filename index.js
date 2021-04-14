6c93e140fe57ebb91e8e65f8fc8619d649745bca
Jared <jaralleg@amazon.com>
2021-04-14 09:45:14 -0400

    built search for user function

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ searchBtn.addEventListener('click', searchForUser)
 function searchForUser(){
    let user = document.getElementById('search').value 
    fetch('https://api.github.com/users/' + user + '/repos', () => {
-       console.log(response)
+        console.log(response)
+        let newLi = document.createElement("li");
+        newLi.innerHTML = user;
+        document.getElementById("user-list").appendChild(newLi);
    })
 }
 
-
+let newLi = document.createElement("li");
+newLi.innerHTML = user;
+document.getElementById("user-list").appendChild(newLi);
 
 
 

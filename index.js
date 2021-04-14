394566a99821190f2f4e692551661a25cb3536cc
Jared <jaralleg@amazon.com>
2021-04-14 10:08:39 -0400

    search function added

--- index.js
+++ index.js
@@ -1,34 +1,16 @@
-
-searches GitHub for users by name and displays the results on the screen
-
+window.addEventListener('DOMContentLoaded', (event) => {
+    console.log('DOM fully loaded and parsed');
 
 const searchBtn = document.querySelector("#github-form > input[type=submit]:nth-child(2)")
 searchBtn.addEventListener('click', searchForUser)
 
 function searchForUser(){
    let user = document.getElementById('search').value 
-   fetch('https://api.github.com/users/' + user + '/repos', () => {
+   fetch('https://api.github.com/search/users?q='+user, () => {
         console.log(response)
-        let newLi = document.createElement("li");
-        newLi.innerHTML = user;
-        document.getElementById("user-list").appendChild(newLi);
-   })
+    })
+    let newLi = document.createElement("li");
+    newLi.innerHTML = user;
+    document.getElementById("user-list").appendChild(newLi);  
 }
-
-let newLi = document.createElement("li");
-newLi.innerHTML = user;
-document.getElementById("user-list").appendChild(newLi);
-
-
-
-
-Clicking on a specific user will show all the repositories for that user.
-
-
-
-The index.html file has a form with a search input. When the form is submitted, 
-it should take the value of the input and search GitHub for user matches using the User Search Endpoint.
-Using the results of the search, display information about the users to the page. (You might include showing their username, 
-    avatar and a link to their profile.)
-Clicking on one of these users should send a request to the User Repos Endpoint and return data about all the repositories for that user.
-Using the response from the Users Repos Endpoint, display all the repositories for that user on the page.
+});
